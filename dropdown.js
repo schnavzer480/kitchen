e64e6a0298f26789f4e26cbd2da48be5ece09136
Christoffer Jonsson <schnavzer@protonmail.com>
2024-11-02 16:18:04 +0100

    cleaned js

--- dropdown.js
+++ dropdown.js
@@ -3,16 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const dropdownMenu = document.querySelector(".dropdown-menu");
     const closeIcon = document.querySelector(".close-icon");
 
-    // Toggle dropdown menu when menu icon is clicked
     menu.addEventListener("click", function() {
         dropdownMenu.classList.toggle("show");
-        menu.classList.toggle("active"); // Toggle active class for the menu icon styling if needed
+        menu.classList.toggle("active");
     });
 
-    // Close dropdown menu when close icon is clicked
     closeIcon.addEventListener("click", function(event) {
         dropdownMenu.classList.remove("show");
-        menu.classList.remove("active"); // Remove active class when closing
-        event.stopPropagation(); // Prevent click from bubbling up
+        menu.classList.remove("active");
+        event.stopPropagation();
     });
 });
